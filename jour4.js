b4e5777ec598c19848e5960245becfda7987ba2b
Syrine Lahouel <syrinelahouel@Syrines-MacBook-Pro.local>
2021-10-01 09:48:06 +0200

    first commit

---
+++ jour4.js
@@ -0,0 +1,8 @@
+// 01 - Object
+
+//- Créez une variable `cat` contenant les clés suivantes : `name` de valeur `"Garfield"`,
+// `age` de valeur `3` et `isCute` de valeur `true`
+//- Affichez `cat` dans la console
+//- Affichez la valeur de `age` dans la console
+//- À l'aide d'une condition, affichez "So cute !" si la valeur de `isCute` est `true`
+
